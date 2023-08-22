b151356d8393f1e1c63e251eed141f46fbf653d9
Seg <sagiv235@gmail.com>
2023-08-22 14:17:49 -0400

    Added python website for k8s exercise

---
+++ k8s_python_web/view.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    server_name = os.uname()[1]
+    return f"""
+            <h1>Greetings!</h1>
+            <p>This server is answering from pod:</p>
+            <p>{server_name}</p>
+            """
+
+if __name__ == '__main__':
+    app.run()
