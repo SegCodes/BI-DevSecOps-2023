6a120663a5bfceda253e0d0ac6d18c6224d59e30
Seg <sagiv235@gmail.com>
2023-08-25 03:09:56 -0400

    Fixed port mapping

--- k8s_python_web/view.py
+++ k8s_python_web/view.py
@@ -13,4 +13,5 @@ def home():
             """
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT',5000))
+    app.run('0.0.0.0', port=port, debug=True)
